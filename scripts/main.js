de4542401edac19e67caa9686b58984a7b3b6012
Samarth Wahal <samarthwahal@hotmail.com>
2016-10-31 18:14:20 -0400

    Boilerplate stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+(function() {
+  var canvas = document.getElementById('timeline');
+
+  var carmackPath = new Path({
+    segments: [[100, 100], [150, 150]]
+  });
+
+  carmackPath.strokeColor = '#3a619e';
+  carmackPath.strokeWidth = 10;
+
+  console.log(canvas);
+})();
