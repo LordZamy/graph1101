085db42882df5640dca764a179043693b5955b2e
Samarth Wahal <samarthwahal@hotmail.com>
2016-10-31 19:22:33 -0400

    Added basic basic fonts and stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,46 @@
 (function() {
-  var canvas = document.getElementById('timeline');
+  var $canvas = $('#timeline');
+  var WIDTH = $canvas.width();
+  var HEIGHT = $canvas.height();
 
   var carmackPath = new Path({
-    segments: [[100, 100], [150, 150]]
-  });
+    segments: [
+      [WIDTH / 12, 200], [WIDTH / 5, HEIGHT / 2],
+      [4 * WIDTH / 5, HEIGHT / 2], [11 * WIDTH / 12, 200]
+    ]
+  })
 
-  carmackPath.strokeColor = '#3a619e';
-  carmackPath.strokeWidth = 10;
+  carmackPath.strokeColor = '#3a619e'
+  carmackPath.strokeWidth = 15
 
-  console.log(canvas);
-})();
+  var carmackHeading = new PointText({
+    point: [view.center.x, 100],
+    content: 'John Carmack',
+    fillColor: '#000000',
+    fontFamily: 'Bungee',
+    fontWeight: 400,
+    fontSize: 42,
+    justification: 'center'
+  })
+
+
+  var romeroHeading = new PointText({
+    point: [view.center.x, HEIGHT - 100],
+    content: 'John Romero',
+    fillColor: '#000000',
+    fontFamily: 'Bungee',
+    fontWeight: 400,
+    fontSize: 42,
+    justification: 'center'
+  })
+
+  var yHalfPath = new Path({
+    segments: [[WIDTH / 2, 0], [WIDTH / 2, HEIGHT]],
+    selected: true
+  })
+
+  var xHalfPath = new Path({
+    segments: [[0, HEIGHT / 2], [WIDTH, HEIGHT / 2]],
+    selected: true
+  })
+})()
